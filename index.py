0059d7101690c9bf1a06d04c19f483a2e25f37c8
manquintana <manuelalejandroquintana@gmail.com>
2026-04-13 18:57:31 +0200

    Added function to simplify plotting

--- index.py
+++ index.py
@@ -50,9 +50,14 @@ df_type = df_type[~(df_type["name"] == "Alleinerziehende ")]
 # ##############
 # SAVE PLOTS
 # ##############
-def save_plot(plt, name):
-    print(f"Plot created into generated_plots > {name}.png")
+def create_yearly_plot(dataframe, title, y_label, name):
+    dataframe.plot(marker="o", figsize=(15,10), linestyle="--")
+    plt.title(title)
+    plt.xlabel("Year")
+    plt.ylabel(y_label)
+    plt.grid(True)
     plt.savefig(f"generated_plots/{name}.png")
+    print(f"Plot created into generated_plots > {name}.png")
     return True
 
 
@@ -61,72 +66,68 @@ def save_plot(plt, name):
 # ################
 
 # 1. Analysis: Household size evolution per year
-df_household_size = df_size[(df_size["merkmal_1"] == "Haushalte") & (df_size["name"] != "Haushalte insgesamt")]
-df_household_size = df_household_size.rename(columns={"name": "Household size"})
-df_household_size = df_household_size.drop(columns = ["merkmal_1"])
-df_household_size = df_household_size.pivot(
+df_size_1 = df_size[(df_size["merkmal_1"] == "Haushalte") & (df_size["name"] != "Haushalte insgesamt")]
+df_size_1 = df_size_1.rename(columns={"name": "Household size"})
+df_size_1 = df_size_1.drop(columns = ["merkmal_1"])
+df_size_1 = df_size_1.pivot(
     index="jahr",
     columns="Household size",
     values="wert"
 )
-df_household_size = df_household_size.rename(columns={
+df_size_1 = df_size_1.rename(columns={
     "eine Person": "1 person",
     "zwei Personen": "2 persons",
     "drei Personen": "3 persons",
     "vier Personen und mehr": "4+ persons"
 })
-df_household_size = df_household_size[["1 person", "2 persons", "3 persons", "4+ persons"]]
-# plot
-df_household_size.plot(marker="o", figsize=(15,10), linestyle="--")
-plt.title("Household size distribution over time")
-plt.xlabel("Year")
-plt.ylabel("Number of Households")
-plt.grid(True)
-#plt.show()
-save_plot(plt, "1.1 - Household size evolution over time")
+df_size_1 = df_size_1[["1 person", "2 persons", "3 persons", "4+ persons"]]
+create_yearly_plot(df_size_1, "Household size distribution over time", "Number of Households", "1.1 - Household size evolution over time")
 
 # 2. Analysis: average household size evolution per year
-df_household_average = df_size[(df_size["name"] == "Durchschnittliche Haushaltsgröße")].drop(columns = ["merkmal_1"])
-df_household_average["name"] = "Average Household size"
-df_household_average = df_household_average.rename(columns={"name": "Household size"})
-df_household_average = df_household_average.pivot(
+df_size_2 = df_size[(df_size["name"] == "Durchschnittliche Haushaltsgröße")].drop(columns = ["merkmal_1"])
+df_size_2["name"] = "Average Household size"
+df_size_2 = df_size_2.rename(columns={"name": "Household size"})
+df_size_2 = df_size_2.pivot(
     index="jahr",
     columns="Household size",
     values="wert"
 )
-# plot
-df_household_average.plot(marker="o", figsize=(15,10), linestyle="--")
-plt.title("Household Size Distribution Over Time")
-plt.xlabel("Year")
-plt.ylabel("Number of Households")
-plt.grid(True)
-#plt.show()
-save_plot(plt, "1.2 - Average household size distribution over time")
+create_yearly_plot(df_size_2, "Average household size distribution over time", "Number of Households", "1.2 - Average household size distribution over time")
 
 
 # ################
 # INITIAL ANALYSIS for df_type (2018-2023)
 # ################
 # 3. Analysis: Average rent % evolution per year
-df_rent_perc = df_type[~(df_type["name"] == "insgesamt")].drop(columns = ["merkmal_1", "einheit"])
-df_rent_perc = df_rent_perc.rename(columns={"wert": "Percentage of income", "name" : "Household Type"})
-df_rent_perc = df_rent_perc.pivot(
+df_type_3 = df_type[~(df_type["name"] == "insgesamt")].drop(columns = ["merkmal_1", "einheit"])
+df_type_3 = df_type_3.rename(columns={"wert": "Percentage of income", "name" : "Household Type"})
+df_type_3 = df_type_3.pivot(
     index="jahr",
     columns="Household Type",
     values="Percentage of income"
 )
-df_rent_perc = df_rent_perc[["Alleinstehende < 65 J. ", "Alleinstehende Rentner", "Paare ohne Kind", "Paare mit Kind(ern)", "Rentnerpaare"]]
-df_rent_perc.plot(marker="o", figsize=(15,10), linestyle="--")
-plt.title("Share of income by Household type spent on housing over time")
-plt.xlabel("Year")
-plt.ylabel("% of net income")
-plt.grid(True)
-#plt.show()
-save_plot(plt, "2.1 - Share of income by Household type spent on housing over time")
+df_type_3 = df_type_3[["Alleinstehende < 65 J. ", "Alleinstehende Rentner", "Paare ohne Kind", "Paare mit Kind(ern)", "Rentnerpaare"]]
+create_yearly_plot(df_type_3, "Share of income by Household type spent on housing over time", "% of income", "2.1 - Share of income by Household type spent on housing over time")
 
 
 # ################
 # INITIAL ANALYSIS for df_situation (2000-2024)
 # ################
+# 4. Analysis: Average sqm per person
+df_situation_4 = df_situation[df_situation["merkmal_1"] == "Durchschnittliche Wohnfläche pro Person"].drop(columns = ["merkmal_1", "einheit"])
+df_situation_4 = df_situation_4[df_situation_4["name"].isin(["1-Personen-Haushalte", "2-Personen-Haushalte", "3-Personen-Haushalte", "4- und Mehr-Personen-Haushalte"])]
+df_situation_4 = df_situation_4.rename(columns={"wert": "sqm", "name" : "Household Composition"})
+df_situation_4 = df_situation_4.pivot(
+    index="jahr",
+    columns="Household Composition",
+    values="sqm"
+)
+create_yearly_plot(df_situation_4, "Average sqm per person over time", "Household Composition", "3.1 - Average living space in sqm per person by household composition over time")
+
+# 5. Analysis: Average room number per person
+
+    
+# 6. Analysis: Property regime
+
 
 
